94d748295972f2cc1753f8aab850f52fce3d3c32
rajas <rajasperception@gmail.com>
2018-12-25 09:08:43 +0530

    lambda example

--- Demo1.py
+++ Demo1.py
@@ -1,2 +1,15 @@
-class Demo1:
+class Demo:
+   
+    """def bigValue(self,a,b):
+        if a>b:
+             print("ais big",a)
+        else:     
+             print("b is big and value is :",b)
+        return      
+obj =Demo()             
+obj.bigValue(10,20)"""
     
+x=lambda a,b:a if a>b else b
+print("big value  is :",x(10,20)) 
+ 
+      
